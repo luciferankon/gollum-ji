0d162e2bad8217a910ea90d0ba0c956b7f64360c
luciferankon <44019225+luciferankon@users.noreply.github.com>
2019-12-17 14:40:09 +0530

    Add redis client to store data

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const Express = require("express");
+const redis = require("redis");
 const app = Express();
 const MongoClient = require("mongodb").MongoClient;
 const { USER, PASSWORD } = process.env;
@@ -43,16 +44,12 @@ const githubUserNames = [
 	"bcalm"
 ];
 
-let db;
-MongoClient.connect(uri, { useUnifiedTopology: true }, (err, database) => {
-	db = database;
-	const port = process.env.PORT || 8080
-	app.listen(port);
-	console.log(`listening on port ${port}`);
+const redisClient = redis.createClient({
+	url: process.env.REDIS_URL,
+	db: 1
 });
 
-const generateDataForTest = (req, res) => {
-	const dbo = db.db("sauron_reporters");
+const enqueueTestData = () => {
 	dbo
 		.collection("events")
 		.find({ job: "test" })
@@ -76,12 +73,12 @@ const generateDataForTest = (req, res) => {
 					})
 					.reverse();
 			});
-			res.setHeader("Access-Control-Allow-Origin", "*");
-			res.send(dataForReports);
+			redisClient.RPOP("testResult");
+			redisClient.LPUSH("testResult", JSON.stringify(dataForReports));
 		});
 };
 
-const generateDataForLint = (req, res) => {
+const enqueueLintData = () => {
 	const dbo = db.db("sauron_reporters");
 	dbo
 		.collection("events")
@@ -101,9 +98,37 @@ const generateDataForLint = (req, res) => {
 					})
 					.reverse();
 			});
-			res.setHeader("Access-Control-Allow-Origin", "*");
-			res.send(dataForReports);
+			redisClient.RPOP("lintResult", err =>
+				console.log(err, "1, Jay hind dosto")
+			);
+			redisClient.LPUSH("lintResult", JSON.stringify(dataForReports), err =>
+				console.log(err, "Jay hind dosto")
+			);
 		});
+};
+
+let db;
+MongoClient.connect(uri, { useUnifiedTopology: true }, (err, database) => {
+	db = database;
+	const port = process.env.PORT || 8080;
+	setInterval(enqueueTestData, 5000);
+	// setInterval(enqueueLintData, 1000);
+	app.listen(port);
+	console.log(`listening on port ${port}`);
+});
+
+const generateDataForTest = (req, res) => {
+	redisClient.RPOP("testResult", (err, reply) => {
+		res.setHeader("Access-Control-Allow-Origin", "*");
+		res.send(JSON.parse(reply));
+	});
+};
+
+const generateDataForLint = (req, res) => {
+	redisClient.RPOP("lintResult", (err, reply) => {
+		res.setHeader("Access-Control-Allow-Origin", "*");
+		res.send(JSON.parse(reply));
+	});
 };
 
 app.get("/test", generateDataForTest);
